dc3f6abc040bedecf3fdc2f4b2b98602fdb51413
lcd97 <danycordero9@gmail.com>
2020-03-10 11:42:03 -0600

    Initial commit

---
+++ app/src/main/java/com/example/evaluacion/MainActivity.java
@@ -0,0 +1,199 @@
+package com.example.evaluacion;
+
+import androidx.annotation.IntegerRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.ln.bean.Recepcion;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.sql.RowSetEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    //DECLARACION DE LOS OBJETOS
+    private EditText NomFuncionario, NomVisitante, CedulaVisitante, MotivoVisita, Documento, Identificador;
+    private Button btnAgregar, btnModificar, btnBuscar;
+
+    //DECLARACION DE VARIABLE PARA ALMACENAR LA FECHA
+    private Date mfecha;
+
+    //DECLARACION DE CONSTANTE LLAVE PARA LAS PREFERENCIAS
+    public static final String KEY_EDITOR = "llave.editor";
+    //VARIABLE PARA ALMACENAR LA PREFERENCIA
+    private SharedPreferences preferencia;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        Identificador = findViewById(R.id.identificador);
+        NomFuncionario = findViewById(R.id.funcionario);
+        NomVisitante = findViewById(R.id.visitante);
+        CedulaVisitante = findViewById(R.id.cedula);
+        MotivoVisita = findViewById(R.id.motivo);
+        Documento = findViewById(R.id.documento);
+        btnAgregar = findViewById(R.id.guardar);
+        btnModificar = findViewById(R.id.modificar);
+        btnBuscar = findViewById(R.id.buscar);
+
+        //
+        final CalendarView fechaObj = findViewById(R.id.calendarView);
+
+        //EVENTO CAMBIO DE FECHA
+        fechaObj.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Calendar c = Calendar.getInstance();
+                c.set(Calendar.YEAR, year);
+                c.set(Calendar.MONTH, month);
+                c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                mfecha = c.getTime();
+            }
+        });
+
+        //DESACTIVAR TODOS LOS BOTONES
+        btnModificar.setEnabled(false);
+
+        //CREAR LAS PREFERENCIAS
+        preferencia = getSharedPreferences(KEY_EDITOR, MODE_PRIVATE);
+
+        //OBTENGO LAS PREFERENCIAS
+        Recepcion recep = Recepcion.getPreferences(preferencia);
+
+        //ASIGNACION DE CAMPOS CON LA ULTIMA PREFERENCIA ALMACENADA
+            //Identificador.setText(String.valueOf(recep.getId()));
+            NomFuncionario.setText(recep.getNomFuncionario());
+            NomVisitante.setText(recep.getNomVisitante());
+            CedulaVisitante.setText(recep.getCedulaVisitante());
+            MotivoVisita.setText(recep.getMotivoVisita());
+            Documento.setText(recep.getDocumento());
+            //fechaObj.setDate(recepcion.getFecha().getTime());
+
+        //EVENTO GUARDAR
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //VALIDACION DE CAMPOS A LLENAR
+                if (Identificador.getText().toString().length() != 0 && NomFuncionario.getText().toString().length() != 0 &&
+                    CedulaVisitante.getText().toString().length() != 0 && NomVisitante.getText().toString().length() != 0 &&
+                    MotivoVisita.getText().toString().length() != 0) {
+                    Recepcion recepcion = new Recepcion();
+
+                    //ASIGNAR LOS VALORES A LA CLASE CREADA
+                    recepcion.setId(Integer.parseInt(Identificador.getText().toString()));
+                    recepcion.setNomFuncionario(NomFuncionario.getText().toString());
+                    recepcion.setNomVisitante(NomVisitante.getText().toString());
+                    recepcion.setCedulaVisitante(CedulaVisitante.getText().toString());
+                    recepcion.setMotivoVisita(MotivoVisita.getText().toString());
+                    recepcion.setDocumento(Documento.getText().toString());
+                    recepcion.setFecha(mfecha);
+
+                    //SI SE GUARDO EN SQLITE
+                    if (recepcion.agregar(MainActivity.this)) {
+                        Identificador.setText("");
+                        NomFuncionario.setText("");
+                        NomVisitante.setText("");
+                        CedulaVisitante.setText("");
+                        MotivoVisita.setText("");
+                        Documento.setText("");
+                        fechaObj.setDate(new Date().getTime());
+
+                    /*ALMACENAR PREFERENCIAS*/
+                    Recepcion rec = new Recepcion();
+                    //rec.setId(Integer.parseInt(Identificador.getText().toString()));
+                    rec.setNomFuncionario(NomFuncionario.getText().toString());
+                    rec.setNomVisitante(NomVisitante.getText().toString());
+                    rec.setCedulaVisitante(CedulaVisitante.getText().toString());
+                    rec.setMotivoVisita(MotivoVisita.getText().toString());
+                    rec.setDocumento(Documento.getText().toString());
+                    //rec.setFecha(mfecha);
+                        //GUARDAR LAS PREFERENCIAS
+                        rec.savePreferences(preferencia);
+
+                        Toast.makeText(MainActivity.this, "Almacenado", Toast.LENGTH_SHORT).show();
+                    } else
+                        Toast.makeText(MainActivity.this, "No almacenado", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(MainActivity.this, "Complete los campos necesarios", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //EVENTO BUSCAR
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Recepcion recepcion = new Recepcion();
+
+                recepcion = recepcion.buscar(MainActivity.this, CedulaVisitante.getText().toString());
+
+                if (recepcion != null) {
+
+                    btnModificar.setEnabled(true);
+                    btnAgregar.setEnabled(false);
+                    Identificador.setEnabled(false);
+
+                    NomFuncionario.setText(String.valueOf(recepcion.getNomFuncionario()));
+                    NomVisitante.setText(String.valueOf(recepcion.getNomVisitante()));
+                    Identificador.setText(String.valueOf(recepcion.getId()));
+                    MotivoVisita.setText(String.valueOf(recepcion.getMotivoVisita()));
+                    Documento.setText(String.valueOf(recepcion.getDocumento()));
+                    fechaObj.setDate(recepcion.getFecha().getTime());
+                } else
+                    Toast.makeText(MainActivity.this, "No existe Cedula", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        //EVENTO MODIFICAR
+        btnModificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Recepcion recepcion = new Recepcion();
+                //ALMACENAR LOS DATOS MODIFICADOS
+                //recepcion.setId(Integer.parseInt(Identificador.getText().toString()));
+                recepcion.setNomFuncionario(NomFuncionario.getText().toString());
+                recepcion.setNomVisitante(NomVisitante.getText().toString());
+                recepcion.setCedulaVisitante(CedulaVisitante.getText().toString());
+                recepcion.setMotivoVisita(MotivoVisita.getText().toString());
+                recepcion.setDocumento(Documento.getText().toString());
+                recepcion.setFecha(mfecha);
+
+                //SI SE MODIFICO LA RECEPCION
+                if(recepcion.modificar(MainActivity.this, CedulaVisitante.getText().toString()))
+                {
+                    //LIMPIAR LOS CAMPOS
+                    Identificador.setText("");
+                    NomFuncionario.setText("");
+                    NomVisitante.setText("");
+                    CedulaVisitante.setText("");
+                    MotivoVisita.setText("");
+                    Documento.setText("");
+                    fechaObj.setDate(new Date().getTime());
+
+                    //ACTIVACION Y DESACTIVACION DE OBJETOS
+                    btnModificar.setEnabled(false);
+                    Identificador.setEnabled(true);
+                    btnAgregar.setEnabled(true);
+                    Toast.makeText(MainActivity.this, "Modificado", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(MainActivity.this, "No Modificado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ ln/src/main/java/com/example/ln/bean/Recepcion.java
@@ -0,0 +1,242 @@
+package com.example.ln.bean;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Recepcion implements Serializable {
+    private int Id;
+    private String NomFuncionario;
+    private String NomVisitante;
+    private String CedulaVisitante;
+    private String MotivoVisita;
+    private Date Fecha;
+    private String Documento;
+
+    //VARIABLE DE PREFERENCIAS
+    public static final String ID = "id";
+    public static final String NOMFUNCIONARIO = "funcionario";
+    public static final String NOMVISITANTE = "visitante";
+    public static final String CEDULAVISITANTE = "cedula";
+    public static final String MOTIVOVISITA = "motivo";
+    public static final String FECHA = "fecha";
+    public static final String DOCUMENTO = "documento";
+
+
+    public Recepcion() {
+        super();
+    }
+
+    public Recepcion(int id, String nomFuncionario, String nomVisitante, String cedulaVisitante, String motivoVisita, Date fecha, String documento) {
+        Id = id;
+        NomFuncionario = nomFuncionario;
+        NomVisitante = nomVisitante;
+        CedulaVisitante = cedulaVisitante;
+        MotivoVisita = motivoVisita;
+        Fecha = fecha;
+        Documento = documento;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNomFuncionario() {
+        return NomFuncionario;
+    }
+
+    public void setNomFuncionario(String nomFuncionario) {
+        NomFuncionario = nomFuncionario;
+    }
+
+    public String getNomVisitante() {
+        return NomVisitante;
+    }
+
+    public void setNomVisitante(String nomVisitante) {
+        NomVisitante = nomVisitante;
+    }
+
+    public String getCedulaVisitante() {
+        return CedulaVisitante;
+    }
+
+    public void setCedulaVisitante(String cedulaVisitante) {
+        CedulaVisitante = cedulaVisitante;
+    }
+
+    public String getMotivoVisita() {
+        return MotivoVisita;
+    }
+
+    public void setMotivoVisita(String motivoVisita) {
+        MotivoVisita = motivoVisita;
+    }
+
+    public Date getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        Fecha = fecha;
+    }
+
+    public String getDocumento() {
+        return Documento;
+    }
+
+    public void setDocumento(String documento) {
+        Documento = documento;
+    }
+
+    //EVENTO AGREGAR A SQLITE
+    public boolean agregar(Context context){
+        boolean band = false;
+
+        SQLiteDatabase myDB = null;
+        SQLiteHelper DBHelper = null;
+
+        try {
+            DBHelper= new SQLiteHelper(context.getFilesDir().getCanonicalPath(),context);
+            myDB= DBHelper.getWritableDatabase();
+
+            ContentValues values= new ContentValues();
+            values.put("Id",Id);
+            values.put("NomFuncionario",NomFuncionario);
+            values.put("NomVisitante",NomVisitante);
+            values.put("CedulaVisitante",CedulaVisitante);
+            values.put("MotivoVisita",MotivoVisita);
+            values.put("Documento",Documento);
+            values.put("Fecha",Fecha.getTime());
+
+            //INSERCION EN LA BD TABLA DE RECEPCION
+            band = myDB.insert("Recepcion",null, values) != -1;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (myDB != null)
+                myDB.close();
+            if (DBHelper != null)
+                DBHelper.close();
+        }
+
+        return band;
+    }
+
+    //METODO PARA MODIFICAR RECEPCION
+    public boolean modificar(Context context, String cedula){
+        boolean band = false;
+
+        SQLiteDatabase myDB = null;
+        SQLiteHelper DBHelper = null;
+
+        try {
+
+            DBHelper= new SQLiteHelper(context.getFilesDir().getCanonicalPath(),context);
+            myDB= DBHelper.getWritableDatabase();
+
+            ContentValues values= new ContentValues();
+            values.put("NomFuncionario",NomFuncionario);
+            values.put("NomVisitante",NomVisitante);
+            values.put("CedulaVisitante",CedulaVisitante);
+            values.put("MotivoVisita",MotivoVisita);
+            values.put("Documento",Documento);
+            values.put("Fecha",Fecha.getTime());
+
+            //MODIFICANDO EL REGISTRO CUYA CEDULA SEA IGUAL AL PARAMETRO CEDULA
+            band = myDB.update("Recepcion",values,"CedulaVisitante=?",new String[]{cedula}) != -1;
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        } finally {
+            if (myDB != null)
+                myDB.close();
+            if (DBHelper != null)
+                DBHelper.close();
+        }
+
+        return band;
+    }
+
+    //METODO PARA BUSCAR UN REGISTRO POR MEDIO DE CEDULA
+    public Recepcion buscar(Context context, String Cedula){
+        Recepcion obj = null;
+        SQLiteDatabase myDataBase=null;
+        SQLiteHelper myDbHelper=null;
+
+        try{
+            myDbHelper= new SQLiteHelper(context.getFilesDir().getCanonicalPath(),context);
+            myDataBase= myDbHelper.getWritableDatabase();
+
+            Cursor cursor= myDataBase.query(true,"Recepcion",null,"CedulaVisitante = ?",new String[]{Cedula},null,null,null,null);
+
+            if(cursor.getCount()>0)
+            {
+                cursor.moveToFirst();
+                while (!cursor.isAfterLast())
+                {
+                    obj = new Recepcion();
+                    obj.setId(cursor.getInt(cursor.getColumnIndex("Id")));
+                    obj.setNomFuncionario(cursor.getString(cursor.getColumnIndex("NomFuncionario")));
+                    obj.setNomVisitante(cursor.getString(cursor.getColumnIndex("NomVisitante")));
+                    obj.setCedulaVisitante(cursor.getString(cursor.getColumnIndex("CedulaVisitante")));
+                    obj.setMotivoVisita(cursor.getString(cursor.getColumnIndex("MotivoVisita")));
+                    obj.setFecha(new Date(cursor.getLong(cursor.getColumnIndex("Fecha"))));
+                    obj.setDocumento(cursor.getString(cursor.getColumnIndex("Documento")));
+                    cursor.moveToNext();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if (myDataBase != null)
+                myDataBase.close();
+            if (myDbHelper != null)
+                myDbHelper.close();
+        }
+
+        return obj;
+    }
+
+    //METODO PARA ALMACENAR LAS PREFERENCIAS
+    public void savePreferences(SharedPreferences preferences){
+        SharedPreferences.Editor editor = preferences.edit();
+
+        //editor.putString(ID, String.valueOf(Id));
+        editor.putString(NOMFUNCIONARIO, NomFuncionario);
+        editor.putString(NOMVISITANTE, NomVisitante);
+        editor.putString(CEDULAVISITANTE, CedulaVisitante);
+        editor.putString(MOTIVOVISITA, MotivoVisita);
+        editor.putString(DOCUMENTO, Documento);
+        //editor.putString(FECHA, String.valueOf(Fecha.getTime()));
+
+        editor.apply();
+    }
+
+    //METODO PARA OBTENER LAS PREFERENCIAS
+    public static Recepcion getPreferences(SharedPreferences preferences) {
+        Recepcion recepcion = new Recepcion();
+
+        //recepcion.setId(Integer.parseInt(preferences.getString(ID,null)));
+        recepcion.setNomFuncionario(preferences.getString(NOMFUNCIONARIO,null));
+        recepcion.setNomVisitante(preferences.getString(NOMVISITANTE,null));
+        recepcion.setCedulaVisitante(preferences.getString(CEDULAVISITANTE,null));
+        recepcion.setMotivoVisita(preferences.getString(MOTIVOVISITA,null));
+        //recepcion.setFecha(preferences.getLong(FECHA));
+        recepcion.setDocumento(preferences.getString(DOCUMENTO,null));
+
+        return recepcion;
+
+    }
+}
